0791889217aac49b7d30a74f01d0dd1324bc3c22
joao <joaovitormachadodev99@gmail.com>
2025-02-06 15:57:07 -0300

    att

---
+++ ex banco de dados1.sql
@@ -0,0 +1,9 @@
+create table alunos (
+	matricula int auto_increment  primary key,
+    nome varchar (30) not null,
+    email varchar (30) not null);
+    insert into alunos (matricula, nome, email) values (null, "João Vitor", "joaoamatsu@gmail.com"),
+    (null, "Rafael Esquisito", "esquisitoide@"),
+    (null, "asfwew", "ASAHIA@.gmail"),
+    (null, "ABC", "AAAAA@gmail"), 
+    (null, "CBA", "asasasAS@GMAI");
