bafc2b444153efe5eaa2dbb1432357966868ebd8
joão Vitor <joaovitormachadodev99@gmail.com>
2025-02-14 17:59:35 -0300

    att
    melhorias

--- main_formulario.py
+++ main_formulario.py
@@ -1,7 +1,8 @@
 import sys 
-from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox
 from untitled import Ui_TelaLogin
 from formulario import Ui_MainWindow
+
 class main(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -17,8 +18,18 @@ class main(QMainWindow):
             if self.telaLogin.lineEdit__senha.text() ==senha:
                 self.telaFormulario = Ui_MainWindow()
                 self.telaFormulario.setupUi(self)
+                self.telaFormulario.pushButton_Finalizar.clicked.connect(self.finalizar_formulario)
+
+    def finalizar_formulario(self):
+        msg= QMessageBox()
+        msg.setWindowTitle("Formulário Concluído! ")
+        msg.setText("Formulário finalizado com sucesso.")
+        msg.setIcon(QMessageBox.Information)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec()
 
 
+            
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = main()
