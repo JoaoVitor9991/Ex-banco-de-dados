baf1869c2b2fd975caecb3aa4a3a6bc1161db1b6
joão Vitor <joaovitormachadodev99@gmail.com>
2025-03-11 18:16:15 -0300

    att

--- novo.py
+++ novo.py
@@ -1,6 +1,12 @@
 import pandas as pd
-print(pd.__version__)
 
-df.set_index('Passagerld')
-inplace=True
+# Carregar o dataset do Titanic
+df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+
+# Definir 'PassengerId' como índice
+df.set_index('PassengerId', inplace=True)
+
+# Exibir as 12 primeiras linhas
 print(df.head(12))
+print(df.columns) # para descobrir as colunas 
+df.values
