bc70d1578a72eeeb7063994da30a131d8e822651
joão Vitor <joaovitormachadodev99@gmail.com>
2025-03-11 17:36:26 -0300

    att

--- eleicao.py
+++ eleicao.py
@@ -1,1 +1,23 @@
 import matplotlib.pyplot as plt 
+
+candidatos = [
+    'Adriane Lopes (PP)',
+    'Rose Modesto (UNIÃO)',
+    'Beto Perereira (PSDB)',
+    'Camila Jara (PT)',
+    'Beto Figueiró (NOVO)',
+    'Luso de Queiroz',
+    'Ubirajara Martins (DC)'
+]
+
+votos = [140913, 131525, 115516, 41966, 10885,3108, 1067]
+
+plt.figure(figsize=(10,6))
+plt.barh(candidatos, votos, color='skyblue')
+plt.xlabel('Número de Votos')
+plt.ylabel('Candidatos')
+plt.title("Quantidade de Votos por Candidato no 1 turno das eleições 2024")
+plt.grid(axis='x', linestyle= '--', alpha=0.7)
+
+
+for index, value in enumerate(votos):
