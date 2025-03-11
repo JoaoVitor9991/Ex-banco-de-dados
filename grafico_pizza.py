d9cc16d3a6e86c11a80105d2f1fe4e94af2e0aa4
joão Vitor <joaovitormachadodev99@gmail.com>
2025-03-11 16:16:33 -0300

    ATT

--- grafico_pizza.py
+++ grafico_pizza.py
@@ -1,3 +1,8 @@
 import matplotlib.pyplot as plt 
 a= (10, 20, 30)
-explode(0.1, 0, 0)
+explode= (0.1, 0, 0)
+labels = ["HB20", "Gol" ,"Fusca"]
+plt.pie(a, explode= explode, labels = labels, autopct='%2f%%', shadow=True)
+plt.title('Carros TANANANAM  TANANAMNAM TANANAM')
+plt.grid(True)
+plt.show()
