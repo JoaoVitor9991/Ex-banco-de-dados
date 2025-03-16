2a499b93bebc475f3d62f2bf744ffa3324040ac4
JoaoVitor9991 <joaovitormachadodev99@gmail.com>
2025-03-15 22:47:21 -0400

    ATIVIDADE

--- ATIVIDADE.py
+++ ATIVIDADE.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv("/kaggle/input/brasileirao-serie-a-2006-2022/brasileirao.csv")
 df_2019 = df[df["season"] ==2019]
-
+#GOLS FEITOS
 mais_gols = df_2019.sort_values(by="goals", ascending=False)
 
 
@@ -16,6 +16,7 @@ plt.ylabel("Gols Marcados")
 plt.xticks(rotation=90)  
 plt.grid(axis="y", linestyle="--", alpha=0.7)
 
+#GOLS SOFRIDOS
 gols_sofridos = df_2019.sort_values(by="goals_taken", ascending=False)
 
 plt.figure(figsize=(12, 6))
@@ -27,6 +28,8 @@ plt.ylabel("Gols sofridos ")
 plt.xticks(rotation=90)
 plt.grid(axis="y", linestyle="--", alpha=0.7)
 
+
+#PONTOS 
 pontuacao_final = df_2019.sort_values(by="points", ascending=False)
 
 plt.figure(figsize=(12, 6))
@@ -37,5 +40,31 @@ plt.xlabel("Times")
 plt.ylabel("Pontuação")
 plt.xticks(rotation=90)
 plt.grid(axis="y", linestyle="--", alpha=0.7)
+
+
+
+vitorias = df_2019.sort_values(by="won", ascending=False)
+
+plt.figure(figsize=(12, 6))
+plt.bar(vitorias["team"], vitorias["won"], color= "green")
+
+plt.title("Quem teve mais vitórias 2019")
+plt.xlabel("Times")
+plt.ylabel("Vitórias")
+plt.xticks(rotation=90)
+plt.grid(axis="y", linestyle="--", alpha=0.7)
+
+
+
+empates = df_2019.sort_values(by="draw", ascending=False)
+
+plt.figure(figsize=(12, 6))
+plt.bar(empates["team"], empates["draw"], color= "red")
+plt.xlabel("Times")
+plt.ylabel("Empates")
+plt.xticks(rotation=90)
+plt.grid(axis='y', linestyle="--", alpha=0.7)
+
+
 
 plt.show()
