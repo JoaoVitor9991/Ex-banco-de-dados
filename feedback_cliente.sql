b7f37cf158417026d08dcf16243a8ca12b131d53
joão Vitor <joaovitormachadodev99@gmail.com>
2025-03-25 14:52:24 -0300

    Update feedback_cliente.sql

--- feedback_cliente.sql
+++ feedback_cliente.sql
@@ -1,25 +1,8 @@
-CREATE DATABASE sistema_feedback;
-
-create table feedbacks (
-	id serial primary key,
-    cliente_id int NOT NULL, 
-    data timestamp default current_timestamp,
-    comentario text not null,
-    resposta TEXT
-    );
-    
-    INSERT INTO feedbacks (cliente_id, comentario) values 
-    (1, 'Ótimo atendimento, estou muito satisfeito!'),
-    (2, 'O serviço poderia ser mais rápido.'),
-    (3, 'Excelente produto, recomendo.'),
-    (4, 'Tive problemas com a entrega do meu pedido.');
-    
-    select * from feedbacks 
-    where data between '2024-03-01' and '2024-03-11';
-    
-    update feedbacks 
-    set resposta = 'Agradecemos seu feedback! Vamos trabalhar na melhoria do serviço.'
-    where id = 2;
-    
-    DELETE FROM feedbacks
-    where id = 4;
+use agenda;
+SELECT nome, telefone FROM contatos WHERE telefone IS NULL OR telefone = '';
+desc contatos;
+ALTER TABLE contatos MODIFY telefone VARCHAR(20) NOT NULL DEFAULT '';
+UPDATE contatos SET telefone = '' WHERE telefone IS NULL;
+SELECT nome, telefone FROM contatos ORDER BY id DESC LIMIT 5;
+SELECT nome, contato FROM contatos;
+SELECT nome, telefone FROM contatos WHERE usuario_id = 1;
